22a8b8f8885f2ec65410272e79473ababc10fca6
Ubirajara Mendes Nunes Junior <ubirajara.mnj@hotmail.com>
2015-10-26 11:06:33 -0200

    Added PermCheck Exercise

---
+++ src/Codility.Lessons/CoutingElements/PermCheck.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.CoutingElements
+{
+    public class PermCheck
+    {
+        public int Solution(int[] A)
+        {
+            if (A.Length == 0) return 0;
+            if (A.Length == 1 && A[0] == 1) return 1;
+            if (A.Length == 1 && A[0] != 1) return 0;
+
+            int maxValue = A.ToList().Max();
+
+            //Generate the Steps Needed
+            List<int> stepsNeeded = new List<int>();
+            for (int i = 1; i <= maxValue; i++)
+                stepsNeeded.Add(i);
+
+            if (A.LongCount() < stepsNeeded.LongCount()) return 0;
+            
+            List<int> AList = A.ToList();
+            foreach (var item in stepsNeeded)
+                if (AList.Contains(item))
+                    AList.Remove(item);
+
+            return (AList.Any()) ? 0 : 1;
+        }
+    }
+}
---
+++ src/Codility.LessonsTests/CoutingElements/PermCheckTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codility.Lessons.CoutingElements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Codility.Lessons.CoutingElements.Tests
+{
+    [TestClass()]
+    public class PermCheckTests
+    {
+        [TestMethod()]
+        public void IsPermutationTest()
+        {
+            int[] array = new int[] { 4, 1, 3, 2 };
+            int expected = 1;
+            Assert.AreEqual(expected, new PermCheck().Solution(array));
+        }
+
+        [TestMethod()]
+        public void IsNotPermutationTest()
+        {
+            int[] array = new int[] { 4, 1, 3 };
+            int expected = 0;
+            Assert.AreEqual(expected, new PermCheck().Solution(array));
+        }
+
+        [TestMethod()]
+        public void IsNotPermutationDueDuplicatedElementTest()
+        {
+            int[] array = new int[] { 4, 1, 3, 2, 1, 3, 2 };
+            int expected = 0;
+            Assert.AreEqual(expected, new PermCheck().Solution(array));
+        }
+
+        [TestMethod()]
+        public void PermutationMinElementTest()
+        {
+            int[] array = new int[] { 1 };
+            int expected = 1;
+            Assert.AreEqual(expected, new PermCheck().Solution(array));
+        }
+
+        [TestMethod()]
+        public void PermutationMin2ElementTest()
+        {
+            int[] array = new int[] { 2 };
+            int expected = 0;
+            Assert.AreEqual(expected, new PermCheck().Solution(array));
+        }
+
+        [TestMethod()]
+        public void NotPermutationTwolementTest()
+        {
+            int[] array = new int[] { 3, 2 };
+            int expected = 0;
+            Assert.AreEqual(expected, new PermCheck().Solution(array));
+        }
+    }
+}
