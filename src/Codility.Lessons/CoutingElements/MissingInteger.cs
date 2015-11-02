fefede615da81064085c84302cbe7a6c5d1081fc
Ubirajara Mendes Nunes Junior <ubirajara.mnj@hotmail.com>
2015-11-02 21:46:51 -0200

    Added MissingInteger Algorithm

---
+++ src/Codility.Lessons/CoutingElements/MissingInteger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Lessons.CoutingElements
+{
+    public class MissingInteger
+    {
+        public int Solution(int[] A)
+        {
+            int[] newArray = new int[A.Length];
+            foreach (var item in A)
+            {
+                if (0 < item && item <= A.Length)
+                    newArray[item - 1] = 1;
+            }
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                if (newArray[i] == 0)
+                    return i + 1;   
+            }
+
+            return A.Length + 1;
+        }
+    }
+}
---
+++ src/Codility.LessonsTests/CoutingElements/MissingIntegerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codility.Lessons.CoutingElements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Codility.Lessons.CoutingElements.Tests
+{
+    [TestClass()]
+    public class MissingIntegerTests
+    {
+        [TestMethod()]
+        public void SolutionTest()
+        {
+            int[] array = new int[] { 1, 3, 6, 4, 1, 2 };
+            int expected = 5;
+            
+            Assert.AreEqual(expected, new MissingInteger().Solution(array));
+        }
+
+        [TestMethod()]
+        public void OneElementTest()
+        {
+            int[] array = new int[] { 1 };
+            int expected = 2;
+
+            Assert.AreEqual(expected, new MissingInteger().Solution(array));
+        }
+
+        [TestMethod()]
+        public void One_2_ElementTest()
+        {
+            int[] array = new int[] { 2 };
+            int expected = 1;
+
+            Assert.AreEqual(expected, new MissingInteger().Solution(array));
+        }
+
+        [TestMethod()]
+        public void One_3_ElementTest()
+        {
+            int[] array = new int[] { 3 };
+            int expected = 1;
+
+            Assert.AreEqual(expected, new MissingInteger().Solution(array));
+        }
+
+        [TestMethod()]
+        public void Extreme_Min_Max_Int_WithMinus_ElementTest()
+        {
+            int[] array = new int[] { int.MinValue, int.MaxValue };
+            int expected = 1;
+
+            Assert.AreEqual(expected, new MissingInteger().Solution(array));
+        }
+
+        [TestMethod()]
+        public void NegativeOnlyElementTest()
+        {
+            int[] array = new int[] { int.MinValue, int.MinValue + 1000 };
+            int expected = 1;
+
+            Assert.AreEqual(expected, new MissingInteger().Solution(array));
+        }
+    }
+}
